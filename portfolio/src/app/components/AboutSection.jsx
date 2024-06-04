982a491a788ca20bde7c9a8a3ef6884a2f2b1719
ahas11 <93216221+ahas11@users.noreply.github.com>
2024-06-04 10:46:42 -0400

    Self portfolio website

---
+++ portfolio/src/app/components/AboutSection.jsx
@@ -0,0 +1,110 @@
+"use client";
+import Image from "next/image";
+import React, { startTransition } from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>JavaScript</li>
+        <li>Next.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>Python</li>
+        <li>C++</li>
+        <li>Java</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>Bachelor's of Computer Science</li>
+        <li>Concordia University</li>
+        <li>2020-2023</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Hobbies",
+    id: "hobbies",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Reading</li>
+        <li>Tennis</li>
+        <li>Travelling</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, setTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/About_Me.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base md:text-lg">
+          I'm a fresh graduate from Concordia University with 
+          a bachelor's degree in Computer Science. Throughout 
+          my academic journey and beyond, I have gained hands-on 
+          experience with a wide range of tools through various projects. 
+          I am passionate about full stack development and love 
+          creating applications that solve real-world problems. 
+          As a quick learner, I constantly seek opportunities to 
+          expand my knowledge and skill set. I thrive in collaborative 
+          environments and am excited to work with others to develop 
+          innovative and impactful applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("hobbies")}
+              active={tab === "hobbies"}
+            >
+              {" "}
+              Hobbies{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -3,8 +3,13 @@ import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
+  {
+    title: "Home",
+    path: "#Home",
+  },
   {
     title: "About",
     path: "#about",
@@ -20,23 +25,23 @@ const navLinks = [
 ];
 
 const Navbar = () => {
-  const [NavbarOpen, setNavbarOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex conatiner lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
           LOGO
         </Link>
-        <div className="mobile menu block md:hidden">
-          {NavbarOpen ? (
-            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
-            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
               <XMarkIcon className="h-5 w-5" />
             </button>
           )}
@@ -52,6 +57,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks}/> : null}
     </nav>
   );
 };
